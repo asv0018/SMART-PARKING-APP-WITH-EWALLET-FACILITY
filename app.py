3a5ec04c96db16fe0f271ae6fd41ed0143992f1a
Shreevallabha A <60250051+asv0018@users.noreply.github.com>
2021-05-15 22:24:09 +0530

    esp8266 backend code is done

--- app.py
+++ app.py
@@ -36,6 +36,205 @@ app = Flask(__name__)
 
 app.secret_key = "THIS-IS-SECRET-KEY"
 
+
+#################################### ESP8266 HARDWARE INTERFACE CODE #################################
+
+@app.route("/check_slot_and_start", methods=["GET"])
+def check_slot_booking():
+    global mycursor, mydb
+    if request.method == "GET":
+        uuid = request.args.get("uuid")
+        slotname = request.args.get("slotname")
+        sql0 = "SELECT occupieduuid, startdatetime FROM slot_database_details WHERE slotname = %s"
+        val0 = (slotname,)
+        mycursor.execute(sql0, val0)
+        data = mycursor.fetchone()
+        mydb.commit()
+        if data != None:
+            occupieduuid = data[0]
+            if occupieduuid == uuid:
+                if data[1]==None:
+                    print("UUID, matches hence, the start time is initiated")
+                    sql1 = "UPDATE slot_database_details SET startdatetime = %s WHERE slotname = %s"
+                    now = datetime.now()
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    val1 = (dt_string, slotname)
+                    mycursor.execute(sql1, val1)
+                    mydb.commit()
+                    payload = {}
+                    payload["status"] = "OK"
+                    payload["message"] = "STARTED_TIMER"
+                    string_data = json.dumps(payload)
+                    return string_data
+
+                else:
+                    payload = {}
+                    payload["status"] = "OK"
+                    payload["message"] = "BILLING_IS_RUNNING"
+                    string_data = json.dumps(payload)
+                    return string_data
+
+            else:
+                payload = {}
+                payload["status"] = "OK"
+                payload["message"] = "MISMATCHED_UUID"
+                print("UUID MISMATCH, CHECK AND TRYAGAIN")
+                string_data = json.dumps(payload)
+                return string_data
+
+        else:
+            print("THE SLOTS ARE EMPTY, BOOK IT FROM APP AND SCAN THE TAG HERE")
+            payload = {}
+            payload["status"] = "NO"
+            payload["message"] = "SLOT_NOT_BOOKED"
+            string_data = json.dumps(payload)
+            return string_data
+
+@app.route("/update_bill", methods=["GET"])
+def update_bill_for_uuid():
+    global mycursor, mydb
+    if request.method == "GET":
+        slotname = request.args.get("slotname")
+        sql0 = "SELECT currentbill, occupieduuid, startdatetime FROM slot_database_details WHERE slotname = %s"
+        val0 = (slotname,)
+        mycursor.execute(sql0, val0)
+        data = mycursor.fetchone()
+        mydb.commit()
+        if data != None:
+            if data[2]!=None:
+                #checking if user has begun session or not
+                uuid = data[1]
+                bill = float(data[0])
+                startdatetime = str(data[2])
+                time = startdatetime.split(" ")[1]
+                hour, min, sec = int(time.split(":")[0]), int(time.split(":")[1]), int(time.split(":")[2])
+                now = datetime.now()
+                t_string = now.strftime("%H:%M:%S")
+                curr_hour = int(t_string.split(":")[0])
+                curr_min = int(t_string.split(":")[1])
+                curr_sec = int(t_string.split(":")[2])
+                present_min = curr_hour*60 + curr_min
+                previous_min = hour*60 + min
+                total_min = present_min - previous_min
+                minutes_in_total = (total_min)/2
+                minutes_in_total = minutes_in_total + 40
+                sql1 = "UPDATE slot_database_details SET currentbill = %s WHERE slotname = %s"
+                val1 = (minutes_in_total, slotname)
+                mycursor.execute(sql1, val1)
+                mydb.commit()
+                
+                payload = {}
+                payload["status"] = "OK"
+                payload["message"] = "BILL_UPDATED"
+                payload["bill"] = minutes_in_total
+                payload["minutes_spent"] = total_min
+                return json.dumps(payload)
+
+            else:
+                payload = {}
+                payload["status"] = "OK"
+                payload["message"] = "BILL_NOT_STARTED"
+                return json.dumps(payload)
+                
+        else:
+            payload = {}
+            payload["status"] = "NO"
+            payload["message"] = "SLOT_NOT_BOOKED"
+            string_data = json.dumps(payload)
+            return string_data
+
+@app.route("/check_and_debit_ewallet", methods=["GET"])
+def check_and_debit_wallet():
+    global mycursor, mydb
+    if request.method == "GET":
+        uuid = request.args.get("uuid")
+        slotname = request.args.get("slotname")
+        sql0 = "SELECT occupieduuid, startdatetime FROM slot_database_details WHERE slotname = %s"
+        val0 = (slotname,)
+        mycursor.execute(sql0, val0)
+        data = mycursor.fetchone()
+        mydb.commit()
+        if data != None:
+            occupieduuid = data[0]
+            if occupieduuid == uuid:
+                if data[1]!=None:
+                    # If the billing is already done means
+                    print("UUID, matches hence, the checkout is initiated")
+                    sql00 = "SELECT * FROM slot_database_details WHERE occupieduuid = %s"
+                    val00 = (uuid,)
+                    mycursor.execute(sql00,val00)
+                    resp = mycursor.fetchone()
+                    mydb.commit()
+                    now = datetime.now()
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+                    starttime, endtime, bill, slotchoosen, ordertedtime = resp[4], dt_string, resp[5], resp[1], resp[3]
+
+                    sql1 = "UPDATE slot_database_details SET occupieduuid = %s, orderedtime = %s, startdatetime = %s, currentbill = %s, status = 'AVAILABLE' WHERE occupieduuid=%s"
+                    val1 = (None, None, None, None, uuid)
+                    mycursor.execute(sql1,val1)
+                    mydb.commit()
+                    sql2 = "SELECT current_balance from user_database WHERE uuid = %s"
+                    val2 = (uuid,)
+                    mycursor.execute(sql2, val2)
+                    balance = mycursor.fetchone()[0]
+                    mydb.commit()
+
+                    balance = float(balance) - float(bill)
+                    sql3 = "UPDATE user_database SET current_balance = %s WHERE uuid = %s"
+                    val3 = (balance, uuid)
+                    mycursor.execute(sql3, val3)
+                    mydb.commit()
+
+                    sql4 = "INSERT INTO all_transaction_details(uuid,datetime,occupiedslot,starttime,endtime,bill,balance,transactdatetime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+                    now = datetime.now()
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    val4 = (uuid, dt_string, slotchoosen, ordertedtime, endtime, bill, balance, dt_string )
+
+                    mycursor.execute(sql4, val4)
+                    mydb.commit()
+
+                    sql5 = "SELECT closing_balance FROM `smart-parking-bank-account-with-transactions` ORDER BY slno DESC LIMIT 1"
+                    mycursor.execute(sql5)
+                    closing_balance = mycursor.fetchone()[0]
+                    mydb.commit()
+
+                    sql6 = "INSERT INTO `smart-parking-bank-account-with-transactions`(date_time, from_account, to_account, amount_credited, closing_balance) VALUES (%s,%s,%s,%s,%s)"
+                    to_account = "121112122212G"
+                    closing_balance = float(closing_balance)+float(bill)
+                    val6 = (dt_string,uuid,to_account, bill, closing_balance)
+                    mycursor.execute(sql6, val6)
+                    mydb.commit()
+
+                    payload = {}
+                    payload["status"] = "OK"
+                    payload["message"] = "CHECKOUT_DONE"
+                    payload["available_balance"] = balance
+
+                    string_data = json.dumps(payload)
+                    return string_data
+
+            else:
+                payload = {}
+                payload["status"] = "OK"
+                payload["message"] = "MISMATCHED_UUID"
+                print("UUID MISMATCH, CHECK AND TRYAGAIN")
+                string_data = json.dumps(payload)
+                return string_data
+
+        else:
+            print("THE SLOTS ARE EMPTY, BOOK IT FROM APP AND SCAN THE TAG HERE")
+            payload = {}
+            payload["status"] = "NO"
+            payload["message"] = "SLOT_NOT_BOOKED"
+            string_data = json.dumps(payload)
+            return string_data
+
+
+
+
+########################################## WEB APP SERVER CODE #######################################
+
 @app.route("/")
 def root_index():
     sql = "SELECT * FROM `smart-parking-bank-account-with-transactions` ORDER BY slno DESC"
@@ -168,32 +367,36 @@ def render_homepage():
         val = (uuid,)
         global mycursor, mydb
         mycursor.execute(sql, val)
-        balance = mycursor.fetchone()[0]
+        balance = mycursor.fetchone()
         mydb.commit()
-        sql = "SELECT `slotname`,`status` FROM `slot_database_details`"
-        mycursor.execute(sql)
-        slots_and_status = mycursor.fetchall()
-        mydb.commit()
-        print(slots_and_status)
-        myresponse = {}
-        myresponse["balance"] = balance
-        sql = "SELECT slotname, orderedtime, startdatetime, currentbill FROM slot_database_details WHERE occupieduuid = %s"
-        val = (uuid,)
-        mycursor.execute(sql, val)
-        resp = mycursor.fetchone()
-        mydb.commit()
-        data = {"data":False}
-        if resp != None:
-            data["data"] = True
-            data["slotname"] = resp[0]
-            data["orderedtime"] = resp[1]
-            data["starttime"] = resp[2]
-            now = datetime.now()
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            data["currenttime"] = dt_string
-            data["currentbill"] = resp[3]
-        
-        return render_template("index.html", session = session, resp = myresponse, data=data, slots_update = slots_and_status)
+        if balance != None:
+            balance = balance[0]
+            sql = "SELECT `slotname`,`status` FROM `slot_database_details`"
+            mycursor.execute(sql)
+            slots_and_status = mycursor.fetchall()
+            mydb.commit()
+            print(slots_and_status)
+            myresponse = {}
+            myresponse["balance"] = balance
+            sql = "SELECT slotname, orderedtime, startdatetime, currentbill FROM slot_database_details WHERE occupieduuid = %s"
+            val = (uuid,)
+            mycursor.execute(sql, val)
+            resp = mycursor.fetchone()
+            mydb.commit()
+            data = {"data":False}
+            if resp != None:
+                data["data"] = True
+                data["slotname"] = resp[0]
+                data["orderedtime"] = resp[1]
+                data["starttime"] = resp[2]
+                now = datetime.now()
+                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                data["currenttime"] = dt_string
+                data["currentbill"] = resp[3]
+            
+            return render_template("index.html", session = session, resp = myresponse, data=data, slots_update = slots_and_status)
+
+        return redirect("/logout")
 
     else:
         return redirect("/login")
